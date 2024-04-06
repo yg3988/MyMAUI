ccf0cfa3ad28d6c4f975b42982c010a983a093bb
yg398 <yg398@HOTP4CK>
2024-04-06 10:04:58 +0900

    Add project files.

---
+++ MyMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
